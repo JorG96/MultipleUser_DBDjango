bf2da7062b97ea0ecae9235d7b2a88294c952e5d
JorG96 <jecamg96@gmail.com>
2022-01-09 23:29:56 -0400

    user views: permission_classes post function

--- user/views.py
+++ user/views.py
@@ -1,3 +1,4 @@
+from os import name
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -8,9 +9,15 @@ class RegisterView():
     permission_classes =(permissions.AllowAny,)
     def post (self,request):
         try:
-            pass
+            data =request.data
+            name=data['name']
+            email=data['email']
+            password=['password']
+            re_password=['re_password']
+            is_realtor =data['is_realtor']
+            
         except:
             return Response(
                 {'error':'Something went wrong when registering an account'},
-                
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
             )
