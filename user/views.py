513b0894b0634f83c00055c6e238925670843387
JorG96 <jecamg96@gmail.com>
2022-01-11 15:08:05 -0400

    users app urls added

--- user/views.py
+++ user/views.py
@@ -1,65 +1,67 @@
-from os import name, stat
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.response import Response
 from rest_framework import permissions, status
 from django.contrib.auth import get_user_model
 from .serializers import UserSerializer
 
 User= get_user_model()
 
-class RegisterView():
+class RegisterView(APIView):
     permission_classes =(permissions.AllowAny,)
-    def post (self,request):
-        try:
-            data =request.data
-            name=data['name']
-            email=data['email']
-            email=email.lower()
-            password=['password']
-            re_password=['re_password']
-            is_realtor =data['is_realtor']
 
-            if is_realtor=='True':
-                is_realtor=True
-            else:
-                is_realtor=False
-            if password==re_password:
-                if len(password)>=8:
-                    if not User.objects.filter(email=email).exists():
-                        if not is_realtor:
-                            User.objects.create_user(name=name,email=email,password=password)
-                            return Response(
-                                {'success':'User created successfully'},
-                                status=status.HTTP_201_CREATED
+    def post(self, request):
+            try:
+                data = request.data
+
+                name = data['name']
+                email = data['email']
+                email = email.lower()
+                password = data['password']
+                re_password = data['re_password']
+                is_realtor = data['is_realtor']
+
+                if is_realtor == 'True':
+                    is_realtor = True
+                else:
+                    is_realtor = False
+
+                if password == re_password:
+                    if len(password) >= 8:
+                        if not User.objects.filter(email=email).exists():
+                            if not is_realtor:
+                                User.objects.create_user(name=name, email=email, password=password)
+
+                                return Response(
+                                    {'success': 'User created successfully'},
+                                    status=status.HTTP_201_CREATED
+                                )
+                            else:
+                                User.objects.create_realtor(name=name, email=email, password=password)
+
+                                return Response(
+                                    {'success': 'Realtor account created successfully'},
+                                    status=status.HTTP_201_CREATED
                                 )
                         else:
-                            User.objects.create_realtor(name=name,email=email,password=password)
                             return Response(
-                                {'success':'Realtor account created successfully'},
-                                status=status.HTTP_201_CREATED
-                                )
+                                {'error': 'User with this email already exists'},
+                                status=status.HTTP_400_BAD_REQUEST
+                            )
                     else:
                         return Response(
-                            {'error':'User this email already exists'},
+                            {'error': 'Password must be at least 8 characters in length'},
                             status=status.HTTP_400_BAD_REQUEST
-                        )                 
+                        )
                 else:
                     return Response(
-                        {'error':'Passwords must be at least 8 characters in length'},
+                        {'error': 'Passwords do not match'},
                         status=status.HTTP_400_BAD_REQUEST
-                    )                   
-            else:
+                    )
+            except:
                 return Response(
-                    {'error':'Passwords do not match'},
-                    status=status.HTTP_400_BAD_REQUEST
+                    {'error': 'Something went wrong when registering an account'},
+                    status=status.HTTP_500_INTERNAL_SERVER_ERROR
                 )
-
-        except:
-            return Response(
-                {'error':'Something went wrong when registering an account'},
-                status=status.HTTP_500_INTERNAL_SERVER_ERROR
-            )
 
 class RetrieveUserView(APIView):
     def get(self,request,format=None):
