5f710257cf11f50da00a0610b993644c4140401f
JorG96 <jecamg96@gmail.com>
2022-01-13 00:19:21 -0400

    listings views: get request completed

--- listing/views.py
+++ listing/views.py
@@ -1,15 +1,137 @@
-from os import stat
-import rest_framework
-from rest_framework import request
-from rest_framework import response
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, permissions
 from .models import Listing
+from .serializers import ListingSerializer
+from django.contrib.postgres.search import SearchVector, SearchQuery
 
-class ManageListing(APIView):
+class ManageListingView(APIView):
     def get(self, request, format=None):
-        pass
+        try:
+            user = request.user
+
+            if not user.is_realtor:
+                return Response(
+                    {'error': 'User does not have necessary permissions for getting this listing data'},
+                    status=status.HTTP_403_FORBIDDEN
+                )
+
+            slug = request.query_params.get('slug')
+
+            if not slug:
+                listing = Listing.objects.order_by('-date_created').filter(
+                    realtor=user.email
+                )
+                listing = ListingSerializer(listing, many=True)
+                
+                return Response(
+                    {'listings': listing.data},
+                    status=status.HTTP_200_OK
+                )
+
+            if not Listing.objects.filter(
+                realtor=user.email,
+                slug=slug
+            ).exists():
+                return Response(
+                    {'error': 'Listing not found'},
+                    status=status.HTTP_404_NOT_FOUND
+                )
+
+            listing = Listing.objects.get(realtor=user.email, slug=slug)
+            listing = ListingSerializer(listing)
+
+            return Response(
+                {'listing': listing.data},
+                status=status.HTTP_200_OK
+            )
+        except:
+            return Response(
+                {'error': 'Something went wrong when retrieving listing or listing detail'},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+
+    def retrieve_values(self, data):
+        title = data['title']
+        slug = data['slug']
+        address = data['address']
+        city = data['city']
+        state = data['state']
+        zipcode = data['zipcode']
+        description = data['description']
+
+        price = data['price']
+        try:
+            price = int(price)
+        except:
+            return -1
+
+        bedrooms = data['bedrooms']
+        try:
+            bedrooms = int(bedrooms)
+        except:
+            return -2
+
+        bathrooms = data['bathrooms']
+        try:
+            bathrooms = float(bathrooms)
+        except:
+            return -3
+
+        if bathrooms <= 0 or bathrooms >= 10:
+            bathrooms = 1.0
+
+        bathrooms = round(bathrooms, 1)
+
+        sale_type = data['sale_type']
+        
+        if sale_type == 'FOR_RENT':
+            sale_type = 'For Rent'
+        else:
+            sale_type = 'For Sale'
+
+        home_type = data['home_type']
+
+        if home_type == 'CONDO':
+            home_type = 'Condo'
+        elif home_type == 'TOWNHOUSE':
+            home_type = 'Townhouse'
+        else:
+            home_type = 'House'
+        
+        main_photo = data['main_photo']
+        photo_1 = data['photo_1']
+        photo_2 = data['photo_2']
+        photo_3 = data['photo_3']
+        is_published = data['is_published']
+
+        if is_published == 'True':
+            is_published = True
+        else:
+            is_published = False
+
+        data = {
+            'title': title,
+            'slug': slug,
+            'address': address,
+            'city': city,
+            'state': state,
+            'zipcode': zipcode,
+            'description': description,
+            'price': price,
+            'bedrooms': bedrooms,
+            'bathrooms': bathrooms,
+            'sale_type': sale_type,
+            'home_type': home_type,
+            'main_photo': main_photo,
+            'photo_1': photo_1,
+            'photo_2': photo_2,
+            'photo_3': photo_3,
+            'is_published': is_published
+        }
+
+        return data
+
     def post(self, request):
         try:
             user = request.user
